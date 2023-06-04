3f0d4724dbe45c8f01624650dd18fe35450a9160
azhang45 <anz0063@gmail.com>
2023-06-04 13:37:50 -0400

    can register combat and mining modes, can deactivate all modes

--- src/main/java/me/azhang/dndplugin/DndPlugin.java
+++ src/main/java/me/azhang/dndplugin/DndPlugin.java
@@ -1,13 +1,72 @@
 package me.azhang.dndplugin;
 
+import me.azhang.dndplugin.commands.ModeCommands;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class DndPlugin extends JavaPlugin {
+public final class DndPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        ModeCommands cmds = new ModeCommands();
+        Bukkit.getPluginManager().registerEvents(this,this);
+        getCommand("combat").setExecutor(cmds);
+        getCommand("mining").setExecutor(cmds);
+        getCommand("deactivate").setExecutor(cmds);
+    }
 
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+        event.setJoinMessage("Welcome to the world, " + event.getPlayer().getDisplayName() + ".");
+    }
+
+    @EventHandler
+    public void onPlayerEggThrow(PlayerEggThrowEvent event){
+        String eggName = event.getEgg().getItem().getItemMeta().getDisplayName();
+        boolean isDice = false;
+        int rollNum = 0;
+        if(eggName.equalsIgnoreCase("d20")){
+            rollNum = (int) (Math.random() * 20) + 1;
+            isDice = true;
+        }
+        else if (eggName.equalsIgnoreCase("d12")){
+            rollNum = (int) (Math.random() * 12) + 1;
+            isDice = true;
+        }
+        else if (eggName.equalsIgnoreCase("d10")){
+            rollNum = (int) (Math.random() * 10) + 1;
+            isDice = true;
+        }
+        else if (eggName.equalsIgnoreCase("d8")){
+            rollNum = (int) (Math.random() * 8) + 1;
+            isDice = true;
+        }
+        else if (eggName.equalsIgnoreCase("d6")){
+            rollNum = (int) (Math.random() * 6) + 1;
+            isDice = true;
+        }
+        else if (eggName.equalsIgnoreCase("d4")){
+            rollNum = (int) (Math.random() * 4) + 1;
+            isDice = true;
+        }
+        else if (eggName.equalsIgnoreCase("d100")){
+            rollNum = (int) (Math.random() * 100) + 1;
+            isDice = true;
+        }
+
+        if(isDice){
+            Player player = event.getPlayer();
+            player.sendMessage("Rolled: " + rollNum);
+        }
+        isDice = false;
     }
 
     @Override
---
+++ src/main/java/me/azhang/dndplugin/commands/ModeCommands.java
@@ -0,0 +1,51 @@
+package me.azhang.dndplugin.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ModeCommands implements CommandExecutor {
+    // [combat, mine, other]
+    public boolean[] modeStatus = new boolean[3];
+    protected boolean modeActive = false;
+
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(!(sender instanceof Player)){
+            return true;
+        }
+
+        Player player = (Player) sender;
+        if(cmd.getName().equalsIgnoreCase("deactivate")){
+            for(int i = 0; i < modeStatus.length; i++){
+                modeStatus[i] = false;
+            }
+            modeActive = false;
+            player.sendMessage("All modes have been deactivated.");
+        }
+        else if(cmd.getName().equalsIgnoreCase("combat")){
+            if(modeActive){
+                player.sendMessage("A mode is already activated and you can only have one active mode at once. Deactive all modes with the command /mode deactive");
+            }
+            else{
+                player.sendMessage("Combat mode activated.");
+                modeStatus[0] = true;
+                modeActive = true;
+            }
+        }
+        else if(cmd.getName().equalsIgnoreCase("mining")){
+            if(modeActive){
+                player.sendMessage("A mode is already activated and you can only have one active mode at once. Deactive all modes with the command /mode deactive");
+            }
+            else{
+                player.sendMessage("Mining mode activated.");
+                modeStatus[1] = true;
+                modeActive = true;
+            }
+        }
+        return true;
+    }
+}
